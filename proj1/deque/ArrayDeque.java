07c155216a85ecc9053686a569ebfc3c00bb113d
lejin <lejinwang12@gmail.com>
2025-11-26 23:11:58 +0800

    done with proj1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,19 +1,19 @@
 package deque;
 
-public class ArrayDeque {
-    private int[] items;
+public class ArrayDeque<LochNess> {
+    private LochNess[] items;
     private int size;
     private int nextfirst;
     private int nextlast;
 
     public ArrayDeque() {
-        items = new int[8];
+        items = (LochNess[]) new Object[8];
         size = 0;
         nextfirst = 3;
         nextlast = 6;
     }
 
-    public void addFirst(int item) {
+    public void addFirst(LochNess item) {
         if (size == items.length) {
             resize(4*items.length);
         }
@@ -22,7 +22,7 @@ public class ArrayDeque {
         nextfirst = getFront(nextfirst);
     }
 
-    public void addLast(int item) {
+    public void addLast(LochNess item) {
         if (size == items.length) {
             resize(4*items.length);
         }
@@ -48,34 +48,34 @@ public class ArrayDeque {
        System.out.println();
     }
 
-    public int removeFirst() {
-        if (size == 0) {return -1;}
+    public LochNess removeFirst() {
+        if (size == 0) {return null;}
         boolean condition = (items.length >= 16) && ((size-1)/items.length < 0.25);
         if(condition){
             resize(items.length/4);
         }
         size--;
         nextfirst = getFront(nextfirst);
-        int value = items[nextfirst];
-        items[nextfirst] = 0;
+        LochNess value = items[nextfirst];
+        items[nextfirst] = null;
         return value;
     }
 
-    public int removeLast() {
-        if (size == 0) {return -1;}
+    public LochNess removeLast() {
+        if (size == 0) {return null;}
         boolean condition = (items.length >= 16) && ((size-1)/items.length < 0.25);
         if(condition){
             resize(items.length/4);
         }
         size--;
         nextlast = getFront(nextlast);
-        int value = items[nextlast];
-        items[nextlast] = 0;
+        LochNess value = items[nextlast];
+        items[nextlast] = null;
         return value;
     }
 
-    public int get(int index){
-        if(index<0 || index>=size){return -1;}
+    public LochNess get(int index){
+        if(index<0 || index>=size){return null;}
         int position = index+getFront(nextfirst);
         if(position>=size){
             position = position-items.length-1;
@@ -85,7 +85,7 @@ public class ArrayDeque {
 
     //resize
     private void resize(int capacity) {
-        int[] temp = new int[capacity];
+        LochNess[] temp = (LochNess[]) new Object[capacity];
         int index = getBack(nextfirst);
 
         for(int i = 0;i<size;i++) {
